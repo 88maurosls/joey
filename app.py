0a99a68ba3c098a7f411e68e63025576f237a0c6
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-11-18 09:55:57 +0100

    Update app.py

--- app.py
+++ app.py
@@ -102,7 +102,7 @@ def process_csv(data, discount_percentage, order_id, view_option):
 
 
 # Interfaccia Streamlit
-st.title("Nike order details")
+st.title("JOEY Nike order details")
 
 # Caricamento del file XLSX
 uploaded_file = st.file_uploader("Carica un file XLSX", type="xlsx")
